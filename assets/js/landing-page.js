3c8e6cdfff5d40235decc0a851920522fd37761b
Bayu Maulana <bayufransdo@gmail.com>
2023-10-12 01:34:27 +0700

    First commit, baru sampe features

---
+++ assets/js/landing-page.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function (e) {
+    const navBar = document.querySelector("nav ul");
+    hamburger.classList.toggle("active");
+    navBar.classList.toggle("active");
+});
+
+// BUAT NAVIGASI
+const links = document.querySelectorAll("nav ul li a");
+links.forEach((link, index) => {
+    if (index === 4) {
+        return;
+    }
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const section = document
+            .getElementById(link.href.split("#")[1])
+            .getBoundingClientRect().top;
+        const bbody = document.body.getBoundingClientRect().top;
+        const destination = section - bbody;
+        // console.log(bbody);
+        // console.log(destination);
+        // console.log(link.getBoundingClientRect().top);
+        window.scrollTo(0, destination - 100);
+    });
+});
