bd66688aa16a35d72b529a0f989bda0ae2bf07cc
Bayu Maulana <bayufransdo@gmail.com>
2023-11-29 16:11:04 +0700

    notification appear with his count

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -6,11 +6,11 @@ try {
   $twoDaysFromNow = date('Y-m-d H:i:s', strtotime('+2 days'));
 
   // Prepare the SQL query
-  $query = "SELECT * FROM activities WHERE date BETWEEN ? AND ?";
+  $query = "SELECT * FROM activities WHERE id_user = ? AND date BETWEEN ? AND ? ";
   $statement = $mysqli->prepare($query);
 
   // Bind the parameters
-  $statement->bind_param('ss', $currentDate, $twoDaysFromNow);
+  $statement->bind_param('sss', $id_user, $currentDate, $twoDaysFromNow);
 
   // Execute the query
   $statement->execute();
@@ -72,6 +72,9 @@ try {
       </a>
       <button class="notifications <?= count($notificationsData) ? 'active' : '' ?>" title="Notifications"
         onclick="toggleNotifications()">
+        <p class="notifications__count">
+          <?= count($notificationsData) ?>
+        </p>
         <svg width="25" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M13.75 28.875H19.25C19.25 30.3875 18.0125 31.625 16.5 31.625C14.9875 31.625 13.75 30.3875 13.75 28.875ZM28.875 26.125V27.5H4.125V26.125L6.875 23.375V15.125C6.875 10.8625 9.625 7.15 13.75 5.9125V5.5C13.75 3.9875 14.9875 2.75 16.5 2.75C18.0125 2.75 19.25 3.9875 19.25 5.5V5.9125C23.375 7.15 26.125 10.8625 26.125 15.125V23.375L28.875 26.125ZM23.375 15.125C23.375 11.275 20.35 8.25 16.5 8.25C12.65 8.25 9.625 11.275 9.625 15.125V24.75H23.375V15.125Z"
@@ -80,7 +83,7 @@ try {
       </button>
       <div class="notifications__content">
         <?php if (!count($notificationsData)): ?>
-          <p class="notifications__content__text">No new notifications</p>
+          <p class="notifications__content__empty">No new notifications</p>
         <?php endif ?>
         <?php if (count($notificationsData)): ?>
           <p><small>
